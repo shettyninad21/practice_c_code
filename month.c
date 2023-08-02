f1608110ca4c8912564fb5e479ea8828b15d5c1c
shettyninad21 <108104429+shettyninad21@users.noreply.github.com>
2023-08-02 10:37:52 +0530

    switch_case_days_month

---
+++ month.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int month,days;
+   printf("\n enter month: ");
+   scanf("%d",&month);
+   switch(month)
+   {
+   case 4:
+   case 6:
+   case 9:
+   case 11:
+       days=30;
+       break;
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        days=31;
+       break;
+    case 2:
+        days=28;
+        break;
+        default:
+        days=0;
+
+   }
+   if(days){
+    printf("number of days = %d",days);
+}
+return 0;
+}
